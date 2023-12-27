b9edf08893c9b196bf5ad039b6ab92f651326362
Marc Kevin Flores <marckevinflores@gmail.com>
2023-12-27 23:14:19 +0800

    fixed issue on app

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
+import { App } from './app/app';
 import { appConfig } from './app/app.config';
 
 
-const bootstrap = () => bootstrapApplication(AppComponent, appConfig);
+const bootstrap = () => bootstrapApplication(App, appConfig);
 
 export default bootstrap;
