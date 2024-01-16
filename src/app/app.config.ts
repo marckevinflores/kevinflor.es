33a9ff6763dedd12d4e5ebbc56be34eec45c7351
Marc Kevin Flores <marckevinflores@gmail.com>
2024-01-17 00:02:32 +0800

    update version from 17.0.8 to 17.0.9

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { PreloadAllModules, provideRouter, withPreloading, withViewTransitions } from '@angular/router';
+import { PreloadAllModules, provideRouter, withPreloading, withViewTransitions, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -8,9 +8,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
     provideHttpClient(withFetch()),
-    provideRouter(routes, withViewTransitions(), withPreloading(PreloadAllModules)
-  //     withInMemoryScrolling({
-  //   scrollPositionRestoration: 'enabled',
-  // })
+    provideRouter(routes, withViewTransitions(), withPreloading(PreloadAllModules),
+      withInMemoryScrolling({
+    scrollPositionRestoration: 'enabled',
+  })
   )]
 };
