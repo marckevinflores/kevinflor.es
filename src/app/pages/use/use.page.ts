a28832645258399b543d7d8a459a22fc7433261c
Marc Kevin Flores <marckevinflores@gmail.com>
2024-01-31 13:00:44 +0800

    update: uses page

--- src/app/pages/use/use.page.ts
+++ src/app/pages/use/use.page.ts
@@ -29,6 +29,7 @@ import { Logo } from '@shared/components/logo/logo';
       </li>
     </ul>
     <h2 class="text-xl font-bold dark:text-white">Typography:</h2>
+    <p class="dark:text-white text-black">For now, I've chosen <a href="https://fonts.google.com/specimen/Inter?query=inter" class="underline" target="_blank" aria-label="Font Style">Inter</a>, a typeface commonly utilized by web designers.</p>
     <div
       class="grid grid-cols-2 sm:grid-cols-4 dark:text-gray-200 text-gray-900 text-center border-y border-gray-500"
     >
@@ -38,17 +39,24 @@ import { Logo } from '@shared/components/logo/logo';
       <p class="border-r border-gray-500 p-4 font-extrabold">Inter Extrabold</p>
     </div>
     <h2 class="text-xl font-bold dark:text-white">Colors:</h2>
-    <p class="dark:text-white text-black">I choose this color palette. The design is a blend of inspiration from various people and websites.
+    <p class="dark:text-white text-black">I chose this color palette, drawing inspiration from various websites to create the design.
     </p>
-    <div class="grid grid-cols-5 sm:grid-cols-10 text-center">
-      @for(v of variants(); track $index){
-        <p [class]="'bg-primary-' + v +' p-4 text-'+ ( v > 400 ? 'white' : 'black')">{{v}}</p>
-      }
+    <div class="grid grid-cols-5 sm:grid-cols-10 text-center select-none">
+      <p class="bg-primary-50 p-4 text-black">50</p>
+      <p class="bg-primary-100 p-4 text-black">100</p>
+      <p class="bg-primary-200 p-4 text-black">200</p>
+      <p class="bg-primary-300 p-4 text-black">300</p>
+      <p class="bg-primary-400 p-4 text-black">400</p>
+      <p class="bg-primary-500 p-4 text-white">500</p>
+      <p class="bg-primary-600 p-4  text-white">600</p>
+      <p class="bg-primary-700 p-4 text-white">700</p>
+      <p class="bg-primary-800 p-4 text-white">800</p>
+      <p class="bg-primary-900 p-4 text-white">900</p>
     </div>
 
     <h2 class="text-xl font-bold dark:text-white">Logo:</h2>
     <p class="text-black dark:text-white">
-      The logo is mixed with the first letter of my full name, M, K, and F letters, to make it short, I just got an idea on Google search.
+    I designed the logo myself, incorporating the first letters of my full name M, K, and F to create a concise design. The inspiration struck me during a Google search.
     </p>
     <div
       class="grid grid-cols-1 sm:grid-cols-2 items-center place-items-center"
